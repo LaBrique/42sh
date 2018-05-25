67f86758b9116bd50167269733d1aee43627d14e
Christophe Chauvot <christophe.chauvot@epitech.eu>
2018-05-25 21:00:40 +0200

    Added echo & quotes handling

--- lib/my/remove_separators.c
+++ lib/my/remove_separators.c
@@ -44,7 +44,6 @@ void	remove_separators_backward(char *str, char *sep, char *oper)
 	size_t i = (int)my_strlen(str) - 1;
 
 	for (; i; i--) {
-
 		if (is_char_present(str[i], oper))
 			sentence = 0;
 		else if (!is_char_present(str[i], sep))
@@ -58,18 +57,23 @@ char	*remove_separators(char *str, char *sep, char *oper)
 {
 	char *clean = NULL;
 	int sentence = 0;
+	int ignore = 0;
 
 	for (size_t i = 0; str[i]; i++) {
-
+		if (str[i] == '"') {
+			ignore = (ignore == 1) ? 0 : 1;
+			str[i] = '\a';
+		}
 		if (is_char_present(str[i], oper))
 			sentence = 0;
 		else if (!is_char_present(str[i], sep))
 			sentence = 1;
-		if ((is_char_present(str[i + 1], sep) || sentence == 0) &&
-			is_char_present(str[i], sep))
+		if (!ignore && (is_char_present(str[i + 1], sep) ||
+			sentence == 0) && is_char_present(str[i], sep))
 			str[i] = '\a';
 	}
 	remove_separators_backward(str, sep, oper);
 	clean = my_strdup_except(str, '\a');
+	my_printf("|%s|\n", clean);
 	return (clean);
 }
