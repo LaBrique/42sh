41a444905f3ce22fddf6b7725b539cdd5ce4bd2a
Camille <camille.villa@epitech.eu>
2018-05-26 14:11:53 +0200

    add error gestion in set_environement.c + add my_strisalpha.c + add my_str_isnum.c

--- unset_environment.c
+++ unset_environment.c
@@ -42,7 +42,8 @@ char	**unsetenv_sh(char **argv, char **envp)
 {
 	int argc = argcounter(++argv);
 
-	if ((argc == 1 && (argv[0][0] == 0 || argv[0][0] == ' ')) || argc == 0) {
+	if (argc == 0) {
+		my_printf("%d  %c\n", argc, argv);
 		my_printf("unsetenv: Too few arguments.\n");
 		return (envp);
 	}
